f754399e17e729f70256b84b344bb64ff9b61551
Madeleine Ouellette <madeleineouellette7@gmail.com>
2022-06-08 22:23:40 -0400

    update

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,7 +5,7 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
-const API_BASE_URL = "https://final-capstone-mo.herokuapp.com/"
+const API_BASE_URL = "https://final-capstone-mo.herokuapp.com"
   //process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 /**
